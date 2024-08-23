d0b0c2061b21d5c772cd709d0ed89d0d7f311d7e
berk <berk_baybas@hotmail.com>
2024-08-23 11:36:36 +0300

    Create modules, add dependencies

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,6 +1,15 @@
+using Catalog;
+using Basket;
+using Ordering;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//module services: catalog, basket, ordering
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
 
